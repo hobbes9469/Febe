23c3f1d3951b57413af7da9483760892918eaa07
hobbes9469 <mattkang202@gmail.com>
2017-07-14 10:39:49 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!febe'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+def runBot():
+    client.run('MzM0NzkzODgwOTgzODMwNTQ5.DEgYnQ.tnwoVLhlu3QaHpPyqNMbzJoMJQg')
+
+if __name__ == "__main__":
+    runBot()
